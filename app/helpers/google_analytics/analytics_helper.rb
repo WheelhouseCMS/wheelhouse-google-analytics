6335e4ec1ab214ab297747fbacb12837baa82249
Sam Pohlenz <sam@sampohlenz.com>
2014-06-13 10:00:52 +0930

    Use a partial to render the tracking code.
    This enables the tracking code to be overridden by the theme if desired.

--- app/helpers/google_analytics/analytics_helper.rb
+++ app/helpers/google_analytics/analytics_helper.rb
@@ -1,19 +1,9 @@
 module GoogleAnalytics::AnalyticsHelper
-  TRACKING_CODE = <<-EOJS
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', '{{CODE}}']);
-    _gaq.push(['_trackPageview']);
-
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-  EOJS
-  
   def google_analytics
-    if Rails.env.production? && site.google_analytics_code? && !controller.env['wheelhouse.preview']
-      javascript_tag(TRACKING_CODE.sub("{{CODE}}", site.google_analytics_code))
-    end
+    render "google_analytics/tracking_code" if render_google_analytics?
+  end
+  
+  def render_google_analytics?
+    Rails.env.production? && site.google_analytics_code? && !controller.env['wheelhouse.preview']
   end
 end
