3f6ee259e4a93204109297e9e38e3924198736c3
Sam Pohlenz <sam@sampohlenz.com>
2014-06-13 11:34:27 +0930

    Add format validation to tracking code

--- app/extensions/wheelhouse/site.rb
+++ app/extensions/wheelhouse/site.rb
@@ -1,3 +1,4 @@
 Wheelhouse::Site.class_eval do
   property :google_analytics_code, String
+  validates_format_of :google_analytics_code, :with => /\AUA-\d{4,9}-\d{1,4}\Z/i, :allow_blank => true
 end
