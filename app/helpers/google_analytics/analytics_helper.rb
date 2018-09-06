8ff59a3b071eefb735974fee658b7f7133aae8be
Sam Pohlenz <sam@sampohlenz.com>
2018-09-06 09:46:40 +0930

    Access env hash via request object

--- app/helpers/google_analytics/analytics_helper.rb
+++ app/helpers/google_analytics/analytics_helper.rb
@@ -2,7 +2,7 @@ module GoogleAnalytics::AnalyticsHelper
   def google_analytics
     render "google_analytics/tracking_code" if render_google_analytics?
   end
-  
+
   def render_google_analytics?
     Rails.env.production? && site.google_analytics_code? && !request.env['wheelhouse.preview']
   end
